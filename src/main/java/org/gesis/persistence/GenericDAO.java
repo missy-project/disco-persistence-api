13925036e92c2331ecd014c1f9b976f6c6a13344
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-06-17 15:43:33 +0200

    adjusted documentation

--- src/main/java/org/gesis/persistence/GenericDAO.java
+++ src/main/java/org/gesis/persistence/GenericDAO.java
@@ -23,6 +23,8 @@ public interface GenericDAO<T>
 	T getById( String urn, boolean lock );
 
 	/**
+	 * Returns the object by the URN-property of Identifiable-class.
+	 * 
 	 * @param urn
 	 * @return
 	 */
@@ -35,9 +37,31 @@ public interface GenericDAO<T>
 	 */
 	List<T> getAll();
 
+	/**
+	 * Returns a list of all objects that are 'similar' to
+	 * <i>exampleInstance</i>. With 'similar' is meant that the list of objects
+	 * must have equal properties as <i>exampleInstance</i>, including relations
+	 * to other objects.
+	 * 
+	 * @param exampleInstance
+	 * @param excludeProperty
+	 * @return
+	 */
 	List<T> getByExample( T exampleInstance, String... excludeProperty );
 
+	/**
+	 * Persists or updates <i>entity</i>.
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	T persist( T entity );
 
+	/**
+	 * Deletes <i>entity</i>.
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	boolean delete( T entity );
 }
