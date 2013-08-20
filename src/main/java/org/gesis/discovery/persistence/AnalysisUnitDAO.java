78680eb60520afd5a80425120ea3904eb400f0c2
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-08-20 10:01:12 +0200

    added getByNotation-method

--- src/main/java/org/gesis/discovery/persistence/AnalysisUnitDAO.java
+++ src/main/java/org/gesis/discovery/persistence/AnalysisUnitDAO.java
@@ -1,5 +1,7 @@
 package org.gesis.discovery.persistence;
 
+import java.util.List;
+
 import org.gesis.discovery.AnalysisUnit;
 import org.gesis.persistence.DAO;
 import org.gesis.persistence.GenericDAO;
@@ -14,4 +16,12 @@ import org.gesis.persistence.GenericDAO;
 public interface AnalysisUnitDAO extends GenericDAO<AnalysisUnit>, DAO
 {
 
+	/**
+	 * Returns a List of AnalysisUnits where property AnalysisUnit.notation
+	 * equals <i>notation</i>.
+	 * 
+	 * @param notation
+	 * @return
+	 */
+	public List<AnalysisUnit> getByNotation( String notation );
 }
--- src/main/java/org/gesis/skos/persistence/ConceptDAO.java
+++ src/main/java/org/gesis/skos/persistence/ConceptDAO.java
@@ -1,5 +1,7 @@
 package org.gesis.skos.persistence;
 
+import java.util.List;
+
 import org.gesis.persistence.DAO;
 import org.gesis.persistence.GenericDAO;
 import org.gesis.skos.Concept;
@@ -14,4 +16,12 @@ import org.gesis.skos.Concept;
 public interface ConceptDAO extends GenericDAO<Concept>, DAO
 {
 
+	/**
+	 * Returns a List of Concepts where property Concept.notation equals
+	 * <i>notation</i>.
+	 * 
+	 * @param notation
+	 * @return
+	 */
+	public List<Concept> getByNotation( String notation );
 }
