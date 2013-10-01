784291c82cb9a6d875f8bc8b7f01a67396309192
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-10-01 13:59:08 +0200

    java.sql.Date -> java.util.Date

--- src/main/java/org/gesis/discovery/persistence/PeriodOfTimeDAO.java
+++ src/main/java/org/gesis/discovery/persistence/PeriodOfTimeDAO.java
@@ -1,9 +1,22 @@
 package org.gesis.discovery.persistence;
 
+import java.util.Date;
+
 import org.gesis.dcterms.PeriodOfTime;
 import org.gesis.persistence.GenericDAO;
 
+/**
+ * @author matthaeus
+ * 
+ */
 public interface PeriodOfTimeDAO extends GenericDAO<PeriodOfTime>
 {
 
+	/**
+	 * Returns a PeriodOfTime-object by it's <i>startDate</i> field value.
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public PeriodOfTime getByStartDate( Date date );
 }
